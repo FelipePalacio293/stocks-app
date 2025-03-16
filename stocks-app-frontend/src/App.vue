82e433d60b921409d11c948e527468b5d24278f0
felipepalacio293 <felipepalacio@javerianacali.edu.co>
2025-03-16 02:47:15 -0500

    add basic configs for vue

--- stocks-app-frontend/src/App.vue
+++ stocks-app-frontend/src/App.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-
+  <div class="min-h-full">
+    <Navbar />
+    <div class="py-10">
+      <main>
+        <div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+          <RouterView />
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
---
+++ stocks-app-frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <div class="mx-auto max-w-7xl">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Consultar listado de stocks</h1>
+      </div>
+    </div>
+    <p class="pt-5">Hola</p>
+  </div>
+</template>
